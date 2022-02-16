60123350aad0c991123ce196f8d23d8075415e19
unknown <bfaidi@gmail.com>
2022-02-16 17:41:09 +0200

    changes

--- src/views/CountryDetails.vue
+++ src/views/CountryDetails.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <div v-for="item in country" :key="item.id" :country="country">
-      <h4>{{ item.name.common }}</h4>
+
+  <div class="card w-75 mx-auto">
+    <div class="card-body">
+      <div class="row" v-for="item in country" :key="item.id" :country="country">
+        <div class="col-md-4">
+          <img :src="item.flags.png">
+         </div>
+         
+        <div class="col-md-8">
+          <h3 style="text-align:left">{{item.name.common}}</h3>
+          <p style="text-align:left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button class="btn btn-success">
+            Conver To {{item.currencies[item.cca3].name}}
+          </button>
+        </div>
+    
+    </div>
     </div>
   </div>
 </template>
